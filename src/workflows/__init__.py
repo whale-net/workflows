9ed4a7a7dc4d8b0ac8b8a120f53802bb5270382d
qmastermoo <cpsaicontact@gmail.com>
2024-11-05 13:08:37 -0500

    add workflow template

--- src/workflows/__init__.py
+++ src/workflows/__init__.py
@@ -2,7 +2,7 @@ import typer
 from workflows.jobs.twitch_to_slack import app as twitch_to_slack_app
 
 app = typer.Typer()
-app.add_typer(twitch_to_slack_app, name="tts")
+app.add_typer(twitch_to_slack_app, name="twitch-to-slack")
 
 
 @app.command()
